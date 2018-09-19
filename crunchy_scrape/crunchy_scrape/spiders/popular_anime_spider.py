9bfdec96ad9a13c581045778e910b3ac81589c7b
Anton <anton.biesheuvel@gmail.com>
2018-09-19 23:09:54 +0200

    Title and ratings scraped

--- crunchy_scrape/crunchy_scrape/spiders/popular_anime_spider.py
+++ crunchy_scrape/crunchy_scrape/spiders/popular_anime_spider.py
@@ -9,7 +9,14 @@ class PopularAnime(scrapy.Spider):
             yield response.follow(href, self.parse_anime)
 
     def parse_anime(self, response):
-        for title in response.css('h1.ellipsis'):
-            yield {
-                'anime-title': title.css('span::text').extract_first(),
-            }
+        def css_extract(query):
+            return response.css(query).extract_first().strip('()')
+
+        yield {
+            'anime-title': css_extract('h1.ellipsis span::text'),
+            '5-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(1) > div:nth-child(3)::text'),
+            '4-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(2) > div:nth-child(3)::text'),
+            '3-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(3) > div:nth-child(3)::text'),
+            '2-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(4) > div:nth-child(3)::text'),
+            '1-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(5) > div:nth-child(3)::text'),
+        }
