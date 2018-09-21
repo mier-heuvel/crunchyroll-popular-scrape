e19906d8bcb46d3f59e85ccbae49895bef8ded1a
Anton <anton.biesheuvel@gmail.com>
2018-09-21 13:55:50 +0200

    Added links to cover art.

--- crunchy_scrape/crunchy_scrape/spiders/popular_anime_spider.py
+++ crunchy_scrape/crunchy_scrape/spiders/popular_anime_spider.py
@@ -10,13 +10,14 @@ class PopularAnime(scrapy.Spider):
 
     def parse_anime(self, response):
         def css_extract(query):
-            return response.css(query).extract_first().strip('()')
+            return response.css(query).extract_first()
 
         yield {
             'anime-title': css_extract('h1.ellipsis span::text'),
-            '5-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(1) > div:nth-child(3)::text'),
-            '4-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(2) > div:nth-child(3)::text'),
-            '3-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(3) > div:nth-child(3)::text'),
-            '2-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(4) > div:nth-child(3)::text'),
-            '1-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(5) > div:nth-child(3)::text'),
+            '5-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(1) > div:nth-child(3)::text').strip('()'),
+            '4-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(2) > div:nth-child(3)::text').strip('()'),
+            '3-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(3) > div:nth-child(3)::text').strip('()'),
+            '2-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(4) > div:nth-child(3)::text').strip('()'),
+            '1-stars': css_extract('ul.rating-histogram:nth-child(3) > li:nth-child(5) > div:nth-child(3)::text').strip('()'),
+            'cover-link': css_extract('.poster::attr(src)'),
         }
